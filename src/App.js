6eb6467ae36c24627ca0ede2f6b6b96613763478
diksha2150487 <130853102+diksha2150487@users.noreply.github.com>
2023-08-13 19:16:57 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,94 @@
+import "./App.css";
+import img1 from "./images/Group 1000007358.png";
+import img2 from "./images/span.platform_ic.png";
+function App() {
+  return (
+    <div>
+      <div className="amazing-shift-main">
+        <div className="genomics-left">
+          <p className="BPM">
+            <span className="core-devo">LET’S BUILD</span>
+            the Amazing Shift4Shop
+          </p>
+          <p className="molwte">Store Together!</p>
+          <p className="audit">
+            Build your High-Ranking Online Store with Our Shift4Shop Experts!
+            Shift4Shop is an easy-to-use, economic, and fully-hosted platform
+            that will simplify your shopping cart by providing quicker
+            development and adaptability. With the best Shift4Shop experts in
+            our team, we are the privileged Shift4Shop organization for you that
+            spends significant time and specializes in the field with
+            excellence.
+          </p>
+          <p className="sky-moon">
+            We are equipped to help the business to grow online because
+            eCommerce achievement is 100% of our core interest. Our Shift4Shop
+            web design architecture incorporates a variety of free core themes,
+            some of which may be ideal for your needs right out of the box.
+          </p>
+        </div>
+        <div className="anatomy-right">
+          <img src={img1} alt="logo" className="koko"></img>
+        </div>
+      </div>
+      {/* ************************************************************************** */}
+      <div className="shift4-development-part">
+        <p className="unity">
+          Shift4Shop Development <span className="vires">Services</span> We
+          Offer
+        </p>
+        <div className="card-sliders">
+          <div className="first1">
+            <div className="same-line-box">
+              <img src={img2} className="watch-time" alt="logo" />
+              <p className="hookup">Shift4Shop Store Setup Service</p>
+            </div>
+            <p className="mozorala">
+              Our mission is to help you begin and grow your Shift4Shop store
+              with our effective Shift4Shop Store Setup Service. As a Shift4Shop
+              Partner, we are intimately familiar with the platform and can
+              assist you with any of your needs.
+            </p>
+          </div>
+          <div className="first1">
+            <div className="same-line-box">
+              <img src={img2} className="watch-time" alt="logo" />
+              <p className="hookup">Shift4Shop Store Setup Service</p>
+            </div>
+            <p className="mozorala">
+              Our mission is to help you begin and grow your Shift4Shop store
+              with our effective Shift4Shop Store Setup Service. As a Shift4Shop
+              Partner, we are intimately familiar with the platform and can
+              assist you with any of your needs.
+            </p>
+          </div>
+          <div className="first1">
+            <div className="same-line-box">
+              <img src={img2} className="watch-time" alt="logo" />
+              <p className="hookup">Shift4Shop Store Setup Service</p>
+            </div>
+            <p className="mozorala">
+              Our mission is to help you begin and grow your Shift4Shop store
+              with our effective Shift4Shop Store Setup Service. As a Shift4Shop
+              Partner, we are intimately familiar with the platform and can
+              assist you with any of your needs.
+            </p>
+          </div>
+          <div className="first1">
+            <div className="same-line-box">
+              <img src={img2} className="watch-time" alt="logo" />
+              <p className="hookup">Shift4Shop Store Setup Service</p>
+            </div>
+            <p className="mozorala">
+              Our mission is to help you begin and grow your Shift4Shop store
+              with our effective Shift4Shop Store Setup Service. As a Shift4Shop
+              Partner, we are intimately familiar with the platform and can
+              assist you with any of your needs.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default App;
