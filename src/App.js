5ac1c09f076615a4c51fee0da92a0089e8c8e0ff
diksha2150487 <130853102+diksha2150487@users.noreply.github.com>
2023-08-14 21:44:07 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import "./App.css";
 import img1 from "./images/Group 1000007358.png";
-import img2 from "./images/span.platform_ic.png";
+import img2 from "./images/yellow-circle.png";
+import img3 from "./images/span.platform_ic.png";
+import img4 from "./images/blue-circle.png";
+import img5 from "./images/pink-circle.png";
+import img6 from "./images/violt-circle.png";
+import img7 from "./images/black-circle.png";
+import img8 from "./images/uiqe-circle.png";
+import img9 from "./images/connection 1.png";
+import img10 from "./images/system-integration 1 (2).png";
+import img11 from "./images/erp 1.png";
+import img12 from "./images/inventory 1.png";
+import img13 from "./images/clipboard 1.png";
+import img14 from "./images/order-fulfillment 1.png";
+import img15 from "./images/max-war.png";
 function App() {
   return (
     <div>
@@ -38,9 +51,9 @@ function App() {
           Offer
         </p>
         <div className="card-sliders">
-          <div className="first1">
+          <div className="faq-yellow-card">
             <div className="same-line-box">
-              <img src={img2} className="watch-time" alt="logo" />
+              <img src={img2} className="troloy" alt="logo" />
               <p className="hookup">Shift4Shop Store Setup Service</p>
             </div>
             <p className="mozorala">
@@ -50,41 +63,306 @@ function App() {
               assist you with any of your needs.
             </p>
           </div>
-          <div className="first1">
+          <div className="shopfiy-slider">
             <div className="same-line-box">
-              <img src={img2} className="watch-time" alt="logo" />
-              <p className="hookup">Shift4Shop Store Setup Service</p>
+              <img src={img3} className="dark-green-img" alt="logo" />
+              <p className="hookup">Shift4Shop Custom Design</p>
             </div>
             <p className="mozorala">
-              Our mission is to help you begin and grow your Shift4Shop store
-              with our effective Shift4Shop Store Setup Service. As a Shift4Shop
-              Partner, we are intimately familiar with the platform and can
-              assist you with any of your needs.
+              We can assist you in enhancing the public image of your business
+              and creating a distinct brand identity by Custom Shift4Shop
+              Design. Customizing Shift4Shop Store Design every few years
+              ensures that your business does not lose out on sales.
             </p>
           </div>
-          <div className="first1">
+          <div className="qeoto-slider">
             <div className="same-line-box">
-              <img src={img2} className="watch-time" alt="logo" />
-              <p className="hookup">Shift4Shop Store Setup Service</p>
+              <img src={img4} className="dark-green-img" alt="logo" />
+              <p className="hookup">Custom Shift4Shop Development</p>
             </div>
             <p className="mozorala">
-              Our mission is to help you begin and grow your Shift4Shop store
-              with our effective Shift4Shop Store Setup Service. As a Shift4Shop
-              Partner, we are intimately familiar with the platform and can
-              assist you with any of your needs.
+              Custom Shift4Shop eCommerce solutions, themes, and API connections
+              are what we do best as a top Shift4Shop Development Agency in the
+              USA and around the world. WebDeskSolution, a leading Shift4Shop
+              Agency, can help you to create and operate a successful business.
             </p>
           </div>
-          <div className="first1">
+          <div className="holqwking-slider ">
             <div className="same-line-box">
-              <img src={img2} className="watch-time" alt="logo" />
-              <p className="hookup">Shift4Shop Store Setup Service</p>
+              <img src={img5} className="dark-green-img" alt="logo" />
+              <p className="hookup">Shift4Shop B2B</p>
             </div>
             <p className="mozorala">
-              Our mission is to help you begin and grow your Shift4Shop store
-              with our effective Shift4Shop Store Setup Service. As a Shift4Shop
-              Partner, we are intimately familiar with the platform and can
-              assist you with any of your needs.
+              We help you to build a beautiful, responsive, high-converting
+              wholesale eCommerce site with greater efficiency, attractiveness,
+              and B2B App. In our role as Shift4Shop experts, we offer
+              enterprise eCommerce solutions that are agile, beautiful and
+              infinitely expandable.
             </p>
+          </div>
+          {/* <div className="allgos-slider ">
+            <div className="same-line-box">
+              <img src={img6} className="dark-green-img" alt="logo" />
+              <p className="hookup">Shift4Shop Migration Experts</p>
+            </div>
+            <p className="mozorala">
+              Our Shift4Shop Migration Experts have created complete Shift4Shop
+              eCommerce migration solutions that make migrating to Shift4Shop
+              simple. Any of our Shift4Shop eCommerce store migration services
+              are excellent for WordPress to Shift4Shop, WooCommerce to
+              Shift4Shop, and Magento to Shift4Shop.
+            </p>
+          </div>
+          <div className="surveyor-slider ">
+            <div className="same-line-box">
+              <img src={img7} className="dark-green-img" alt="logo" />
+              <p className="hookup">Shift4Shop Migration Experts</p>
+            </div>
+            <p className="mozorala">
+              Our Shift4Shop Migration Experts have created complete Shift4Shop
+              eCommerce migration solutions that make migrating to Shift4Shop
+              simple. Any of our Shift4Shop eCommerce store migration services
+              are excellent for WordPress to Shift4Shop, WooCommerce to
+              Shift4Shop, and Magento to Shift4Shop.
+            </p>
+          </div>
+          <div className="maters-slider ">
+            <div className="same-line-box">
+              <img src={img8} className="dark-green-img" alt="logo" />
+              <p className="hookup">Shift4Shop Migration Experts</p>
+            </div>
+            <p className="mozorala">
+              Our Shift4Shop Migration Experts have created complete Shift4Shop
+              eCommerce migration solutions that make migrating to Shift4Shop
+              simple. Any of our Shift4Shop eCommerce store migration services
+              are excellent for WordPress to Shift4Shop, WooCommerce to
+              Shift4Shop, and Magento to Shift4Shop.
+            </p>
+          </div> */}
+        </div>
+      </div>
+      {/* ************************************************************************ */}
+      <div className="snip1390">
+        <p className="laborty">
+          Shift4Shop <span className="colegion">Solutions & Integrations</span>{" "}
+          For Your Store
+        </p>
+        <div className="differtion">
+          <div className="card-virus-main-lofo">
+            <div class="card-virus-lg">
+              <div class="covid-virus">
+                <img src={img9} alt="logo" />
+              </div>
+              <div class="content">
+                <div class="title">Shift4Shop Omnichannel</div>
+                <p className="sub-title">
+                  Omnichannel commerce enables you to sell wherever your
+                  customers buy, so long as you have an internet connection.
+                  Amazon, eBay, Facebook, Instagram, and more are all fully
+                  integrated within the platform. The Shift4Shop API lets you
+                  handle listings, orders, and inventory.
+                </p>
+              </div>
+            </div>
+            <div class="card-virus-lg" id="gwon">
+              <div class="covid-virus">
+                <img src={img10} alt="logo" />
+              </div>
+              <div class="content">
+                <div class="title">Shift4Shop ERP Integration</div>
+                <p className="sub-title">
+                  The Shift4Shop ERP integration can track customer purchases,
+                  conversions, and traffic. A profitable company requires
+                  Shift4Shop ERP integration to properly smoothen its whole
+                  business operations and processes. We can help you integrate
+                  your Shift4Shop store with Quickbooks, Sage, dynamics 365,
+                  Netsuite, or Fishbowl.
+                </p>
+              </div>
+            </div>
+            <div class="card-virus-lg" id="dolll">
+              <div class="covid-virus">
+                <img src={img11} alt="logo" />
+              </div>
+              <div class="content">
+                <div class="title">Shift4Shop CRM Integration</div>
+                <p className="sub-title">
+                  To better manage your customers and provide a great user
+                  experience, you require Shift4Shop CRM integrations. You can
+                  also use CRM integration, such as Salesforce, Zoho, or HubSpot
+                  to centralize customer data and share it with your staff.
+                </p>
+              </div>
+            </div>
+            <div class="card-virus-lg" id="bublle">
+              <div class="covid-virus">
+                <img src={img12} alt="logo" />
+              </div>
+              <div class="content">
+                <div class="title">Shift4Shop Inventory Management</div>
+                <p className="sub-title">
+                  Keeping track of your inventory can help you avoid lost,
+                  refunded, and damaged products. It also improves customer
+                  service and saves overall inventory expenses. We can also
+                  assist you in integrating your Shift4Shop Inventory Management
+                  Systems with your multi- channel marketing efforts.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="card-main-lefo-second">
+            <div class="card-virus-lg">
+              <div class="covid-virus">
+                <img src={img13} alt="logo" />
+              </div>
+              <div class="content">
+                <div class="title">Shift4Shop Omnichannel</div>
+                <p className="sub-title">
+                  Omnichannel commerce enables you to sell wherever your
+                  customers buy, so long as you have an internet connection.
+                  Amazon, eBay, Facebook, Instagram, and more are all fully
+                  integrated within the platform. The Shift4Shop API lets you
+                  handle listings, orders, and inventory.
+                </p>
+              </div>
+            </div>
+            <div class="card-virus-lg" id="gwon">
+              <div class="covid-virus">
+                <img src={img10} alt="logo" />
+              </div>
+              <div class="content">
+                <div class="title">Shift4Shop ERP Integration</div>
+                <p className="sub-title">
+                  The Shift4Shop ERP integration can track customer purchases,
+                  conversions, and traffic. A profitable company requires
+                  Shift4Shop ERP integration to properly smoothen its whole
+                  business operations and processes. We can help you integrate
+                  your Shift4Shop store with Quickbooks, Sage, dynamics 365,
+                  Netsuite, or Fishbowl.
+                </p>
+              </div>
+            </div>
+            <div class="card-virus-lg" id="dolll">
+              <div class="covid-virus">
+                <img src={img14} alt="logo" />
+              </div>
+              <div class="content">
+                <div class="title">Shift4Shop CRM Integration</div>
+                <p className="sub-title">
+                  To better manage your customers and provide a great user
+                  experience, you require Shift4Shop CRM integrations. You can
+                  also use CRM integration, such as Salesforce, Zoho, or HubSpot
+                  to centralize customer data and share it with your staff.
+                </p>
+              </div>
+            </div>
+            <div class="card-virus-lg" id="bublle">
+              <div class="covid-virus">
+                <img src={img10} alt="logo" />
+              </div>
+              <div class="content">
+                <div class="title">Shift4Shop Inventory Management</div>
+                <p className="sub-title">
+                  Keeping track of your inventory can help you avoid lost,
+                  refunded, and damaged products. It also improves customer
+                  service and saves overall inventory expenses. We can also
+                  assist you in integrating your Shift4Shop Inventory Management
+                  Systems with your multi- channel marketing efforts.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* /*********************************************************************** */}
+      <div className="cngo-protctive">
+        <p className="diamond">
+          Why <span className="coffigo">Choose Us</span> for Shift4Shop
+          Development Services?
+        </p>
+        <div className="water-card-part">
+          <div className="Months-step-main">
+            <div className="provegn">
+              <p className="expert">Certified Experts</p>
+              <hr id="short-viggly-line" />
+              <p className="mozo">
+                We are a Shift4Shop website design and development company
+                comprising certified professionals, all of whom are experts in
+                their respective fields with substantial years of professional
+                experience.
+              </p>
+            </div>
+            <div className="provegn" id="blopo">
+              <p className="expert">Certified Experts</p>
+              <hr id="semi" />
+              <p className="mozo">
+                We are a Shift4Shop website design and development company
+                comprising certified professionals, all of whom are experts in
+                their respective fields with substantial years of professional
+                experience.
+              </p>
+            </div>
+            <div className="provegn" id="mack">
+              <p className="expert">Certified Experts</p>
+              <hr id="faho" />
+              <p className="mozo">
+                We are a Shift4Shop website design and development company
+                comprising certified professionals, all of whom are experts in
+                their respective fields with substantial years of professional
+                experience.
+              </p>
+            </div>
+          </div>
+          <div className="third-with-main-section">
+            <div className="provegn" id="moting">
+              <p className="expert">Certified Experts</p>
+              <hr id="shoot" />
+              <p className="mozo">
+                We are a Shift4Shop website design and development company
+                comprising certified professionals, all of whom are experts in
+                their respective fields with substantial years of professional
+                experience.
+              </p>
+            </div>
+            <div className="provegn" id="grey-main">
+              <p className="expert">Certified Experts</p>
+              <hr id="jalko" />
+              <p className="mozo">
+                We are a Shift4Shop website design and development company
+                comprising certified professionals, all of whom are experts in
+                their respective fields with substantial years of professional
+                experience.
+              </p>
+            </div>
+            <div className="provegn" id="wahaa">
+              <p className="expert">Certified Experts</p>
+              <hr id="thems" />
+              <p className="mozo">
+                We are a Shift4Shop website design and development company
+                comprising certified professionals, all of whom are experts in
+                their respective fields with substantial years of professional
+                experience.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* ********************************************************************************************** */}
+      <div className="group-main">
+        <p className="sicso">
+          Our
+          <span className="high-over">WordPress</span>Web Development{" "}
+          <span className="high-over"> Portfolio </span>
+        </p>
+        <div className="jio">
+          <img src={img15} id="waar" alt="logo"></img>
+          <div className="contact">
+            <p className="in-house">
+              TECHNOLOGY SHOULD EXPAND YOUR POTENTIAL,NOT LIMIT IT
+            </p>
+            <div className="front-icon">
+              <p className="website">View Website</p>
+            </div>
           </div>
         </div>
       </div>
